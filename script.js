25332e979bdba7ea8d638b0807ee29323ee31bd2
Sean Andrews <sandrews2332@gmail.com>
2021-07-08 14:52:51 -0400

    Create grid and mouseover functionalities

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('style');
+const game = document.getElementById("game");
+
+function makeGrid(rows, columns) {
+    game.style.setProperty('--grid-rows', rows);
+    game.style.setProperty('--grid-columns', columns);
+    for (c = 0; c < (rows * columns); c++) {
+        let cell = document.createElement('div');
+        game.appendChild(cell).className = "grid-item";
+    };
+};
+
+makeGrid(30, 30);
+
+const gridItem = document.querySelectorAll('.grid-item');
+console.log(gridItem);
+
+gridItem.forEach(function(grab) {
+    grab.addEventListener('mouseover', function() {
+        grab.style.backgroundColor = "black";
+    });
+});
