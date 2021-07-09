6c5ad981bd4101a49b5b27069bb87fcc80da2c3d
Sean Andrews <sandrews2332@gmail.com>
2021-07-09 13:18:21 -0400

    Fix grid button functionality and color grid functionality

--- script.js
+++ script.js
@@ -2,19 +2,51 @@
 
 const gameBoard = document.getElementById('game');
 
+let num = 10;
+makeGrid(num, num);
+
 function makeGrid(rows, cols) {
     gameBoard.style.setProperty('--grid-rows', rows);
     gameBoard.style.setProperty('--grid-cols', cols);
     for (let i=0; i < rows * cols; i++) {
         let cell = document.createElement('div');
-        gameBoard.appendChild(cell).className = 'square'
+        gameBoard.appendChild(cell).className = 'square';
     };
+    colorGrid();
 };
 
-makeGrid(16, 16);
+// Change color of each div in game grid on mouse hover
+function colorGrid(){
+const box = document.querySelectorAll('.square');
 
-// Change color of each div in game grid
+box.forEach((pixel) => {
+    pixel.addEventListener('mouseover', event => {
+        pixel.style.backgroundColor = 'black';
+    });
+});
+}
 
 // Create button to reset board and input new grid
 
+const resetBtn = document.getElementById('clear');
+
+resetBtn.addEventListener('click', event => {
+    resetBoard();
+});
+
+function resetBoard() {
+    let num = prompt("Pick a number between and 64: ");
+        if (num > 64) {
+            alert("That number was too high");
+            resetBoard();
+        } else if (num < 1) {
+            alert("That number was too low");
+            resetBoard();
+        } else {
+            while (gameBoard.firstChild) {
+                gameBoard.removeChild(gameBoard.firstChild);
+            }
+            makeGrid(num, num);
+        }            
+}
 // Create button to change color of sketch
