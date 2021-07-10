3582bbe579f114e97742c4d9d768333848ad612e
Sean Andrews <sandrews2332@gmail.com>
2021-07-10 08:35:00 -0400

    Create random color button and clean code

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-// Create a game grid of divs 
+// Initial variables
 
 const gameBoard = document.getElementById('game');
+let randomColor;
+let num = 16;
+const randomize = document.getElementById('randomize');
+const resetBtn = document.getElementById('clear');
 
-let num = 10;
+// Resets the board when user presses the 'clear' button
+
+resetBtn.addEventListener('click', event => {
+    resetBoard();
+});
+
+// Randomizes the color when user presses the 'Wild Colors!' button
+
+randomize.addEventListener('click', event => {
+    randomColor = true;
+    colorGrid();
+});
+
+// Initial setup of grid
+
 makeGrid(num, num);
+
+// Function to create the grid and call color function
 
 function makeGrid(rows, cols) {
     gameBoard.style.setProperty('--grid-rows', rows);
@@ -15,24 +35,22 @@ function makeGrid(rows, cols) {
     colorGrid();
 };
 
-// Change color of each div in game grid on mouse hover
+// Color function to change color of sketch
+
 function colorGrid(){
-const box = document.querySelectorAll('.square');
+    let box = document.querySelectorAll('.square');
+        if (randomColor === true) {
+           wildColor();
+        } else {
+                box.forEach((pixel) => {
+                pixel.addEventListener('mouseover', event => {
+                    pixel.style.backgroundColor = document.getElementById('color').value;
+            });
+        });    
+        };
+    }
 
-box.forEach((pixel) => {
-    pixel.addEventListener('mouseover', event => {
-        pixel.style.backgroundColor = document.getElementById('color').value;
-    });
-});
-}
-
-// Create button to reset board and input new grid
-
-const resetBtn = document.getElementById('clear');
-
-resetBtn.addEventListener('click', event => {
-    resetBoard();
-});
+// Function to reset the board based on user input
 
 function resetBoard() {
     let num = prompt("Pick a number between and 64: ");
@@ -43,10 +61,39 @@ function resetBoard() {
             alert("That number was too low");
             resetBoard();
         } else {
+            let box = document.querySelectorAll('.square');
+            randomColor = false;
+            box.forEach((pixel) => {
+                pixel.addEventListener('click', event => {
+                    pixel.style.backgroundColor = 'black';
+                })
+            });
             while (gameBoard.firstChild) {
                 gameBoard.removeChild(gameBoard.firstChild);
             }
             makeGrid(num, num);
-        }            
+        };            
+};
+
+//Function 1 of 2 to set up randomizing color
+
+function wildColor() {
+    let box = document.querySelectorAll('.square');
+    box.forEach((pixel) => {
+        pixel.addEventListener('mouseover', event => {
+            pixel.style.backgroundColor = pixelColor();
+        })
+    })
 }
-// Create button to randomize color of sketch
+
+// Function 2 of 2 to choose the pixel color at random
+
+function pixelColor() {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    return "rgb(" + r + ", " + g + ", " + b + ")"; 
+}
+
+
+
